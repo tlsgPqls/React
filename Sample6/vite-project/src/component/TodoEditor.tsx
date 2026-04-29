bf884f42019278686dadeb16f3a8c419d4b627a4
Shin <hoddi98@gmail.com>
2026-04-29 14:36:07 +0900

    ddd

--- Sample6/vite-project/src/component/TodoEditor.tsx
+++ Sample6/vite-project/src/component/TodoEditor.tsx
@@ -1,15 +1,27 @@
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
 import "./TodoEditor.css";
 interface TodoEditorProps {
   onCreate: (content: string) => void;
 }
 function TodoEditor({ onCreate }: TodoEditorProps) {
   const [content, setContent] = useState<string>("");
+  const inputRef = useRef<HTMLInputElement>(null);
   const onChangeContent = (e: React.ChangeEvent<HTMLInputElement>) => {
     setContent(e.target.value);
   };
   const onSubmit = () => {
+    if (!content) {
+      inputRef.current?.focus();
+      return;
+    }
     onCreate(content);
+    setContent("");
+  };
+
+  const onKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === "Enter") {
+      onSubmit();
+    }
   };
   return (
     <div className="TodoEditor">
@@ -19,6 +31,9 @@ function TodoEditor({ onCreate }: TodoEditorProps) {
           type="text"
           placeholder="새로운 Todo...."
           onChange={onChangeContent}
+          ref={inputRef}
+          onKeyDown={onKeyDown}
+          value={content}
         />
         <button onClick={onSubmit}>후기</button>
       </div>
--- Sample6/vite-project/src/component/TodoList.tsx
+++ Sample6/vite-project/src/component/TodoList.tsx
@@ -2,10 +2,24 @@ import "./TodoList.css";
 import "./TodoItem";
 import TodoItem from "./TodoItem";
 import { type Todo } from "../App";
+import { useState, type ChangeEvent } from "react";
 interface TodoListProps {
   todos: Todo[];
 }
 function TodoList({ todos }: TodoListProps) {
+  const [search, setSearch] = useState("");
+
+  const onChangeSearch = (e: ChangeEvent<HTMLInputElement>) => {
+    setSearch(e.target.value);
+  };
+
+  const getSearchResult = () => {
+    return search === ""
+      ? todos
+      : todos.filter((todo) =>
+          todo.content.toLowerCase().includes(search.toLowerCase()),
+        );
+  };
   return (
     <div className="TodoList">
       <h4>Todo List</h4>
@@ -13,9 +27,10 @@ function TodoList({ todos }: TodoListProps) {
         type="text"
         className="searchbar"
         placeholder="검색어를 입력하세요..."
+        onChange={onChangeSearch}
       />
       <div>
-        {todos.map((todo) => (
+        {getSearchResult().map((todo) => (
           <TodoItem todo={todo} key={todo.id} />
         ))}
       </div>
