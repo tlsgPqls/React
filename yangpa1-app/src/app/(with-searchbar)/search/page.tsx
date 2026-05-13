b41b2b54286aa5963677036384edf058c704198b
Shin <hoddi98@gmail.com>
2026-05-13 17:02:11 +0900

    project1

--- yangpa1-app/src/app/(with-searchbar)/search/page.tsx
+++ yangpa1-app/src/app/(with-searchbar)/search/page.tsx
@@ -8,11 +8,12 @@ export default async function Page({
   searchParams: Promise<{ q: string }>;
 }) {
   const { q } = await searchParams;
-  let url = `${ENV.API_URL}/sales`;
-  if (q) {
-    url += `?q=${q}`;
-  }
+  let url = `${ENV.API_URL}/${q}`;
+  // if (q) {
+  //   url += `?q=${q}`;
+  // }
   const response = await fetch(url);
+  console.log(url);
   const data = await response.json();
   const sales: SaleData[] = data.documents;
   return (
